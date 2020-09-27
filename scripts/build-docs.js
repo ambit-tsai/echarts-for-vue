08c9c25f8e8bf525cddba9f1812231e46592c11e
ambit <ambit_tsai@qq.com>
2020-09-27 14:43:32 +0800

    docs: rebuild

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,45 +1,13 @@
 const path = require('path');
 const ROOT = path.resolve(__dirname, '../');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const config = require('./test-vue2');
 
 
 module.exports = {
-    mode: 'development',
-    devtool: 'cheap-module-inline-source-map',
-    context: ROOT,
-    entry: `${ROOT}/test/main-vue3.js`,
+    ...config,
+    mode: 'production',
     output: {
         path: `${ROOT}/docs`,
-        filename: 'test-vue3.js',
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
-                },
-            },
-        }, {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-            ],
-        }],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: `${ROOT}/test/index.html`,
-            filename: 'index.html',
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-    ],
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
+        filename: 'index.js',
     },
 };
