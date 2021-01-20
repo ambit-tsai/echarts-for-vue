79fdc0e015c10d1306b68fd0a616dcf5f909189f
ambit <ambit_tsai@qq.com>
2021-01-20 15:57:30 +0800

    fix: call `setOption` after rendering

--- src/LifecycleHooks.js
+++ src/LifecycleHooks.js
@@ -3,23 +3,23 @@ export function getHooks(echarts) {
     return {
         mounted() {
             const ctx = this;
-            ctx.$nextTick(() => {
-                const inst = echarts.init(ctx.$el, ctx.initTheme, ctx.initOpts);
-                ctx.$data._private.dynamic.inst = inst;
-                
-                if (ctx.loading) {
-                    inst.showLoading(ctx.loadingType, ctx.loadingOpts)
-                }
-                if (ctx.option) {
+            const inst = echarts.init(ctx.$el, ctx.initTheme, ctx.initOpts);
+            ctx.$data._private.dynamic.inst = inst;
+            if (ctx.loading) {
+                inst.showLoading(ctx.loadingType, ctx.loadingOpts)
+            }
+            if (ctx.events) {
+                ctx.events.forEach(args => inst.on.apply(inst, args));
+            }
+            if (ctx.autoResize) {
+                ctx.addResizeListener();
+            }
+            if (ctx.option) {
+                // Wait for rendering
+                setTimeout(() => {
                     ctx.setOption(ctx.option, ctx.setOptionOpts);
-                }
-                if (ctx.events) {
-                    ctx.events.forEach(args => inst.on.apply(inst, args));
-                }
-                if (ctx.autoResize) {
-                    ctx.addResizeListener();
-                }
-            });
+                });
+            }
         },
         
         activated() {
