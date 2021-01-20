54888e973227bbf6785d2ea47ee5b02cf8470bff
ambit <ambit_tsai@qq.com>
2021-01-20 10:45:49 +0800

    fix: initialize when DOM ready

--- src/LifecycleHooks.js
+++ src/LifecycleHooks.js
@@ -2,21 +2,23 @@
 export function getHooks(echarts) {
     return {
         mounted() {
-            const inst = echarts.init(this.$el, this.initTheme, this.initOpts);
-            this.$data._private.dynamic.inst = inst;
-            
-            if (this.loading) {
-                inst.showLoading(this.loadingType, this.loadingOpts)
-            }
-            if (this.option) {
-                this.setOption(this.option, this.setOptionOpts);
-            }
-            if (this.events) {
-                this.events.forEach(args => inst.on(...args));
-            }
-            if (this.autoResize) {
-                this.addResizeListener();
-            }
+            this.$nextTick(() => {
+                const inst = echarts.init(this.$el, this.initTheme, this.initOpts);
+                this.$data._private.dynamic.inst = inst;
+                
+                if (this.loading) {
+                    inst.showLoading(this.loadingType, this.loadingOpts)
+                }
+                if (this.option) {
+                    this.setOption(this.option, this.setOptionOpts);
+                }
+                if (this.events) {
+                    this.events.forEach(args => inst.on(...args));
+                }
+                if (this.autoResize) {
+                    this.addResizeListener();
+                }
+            });
         },
         
         activated() {
